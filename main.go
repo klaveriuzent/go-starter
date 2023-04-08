e4d2f0c93eee6cedf9388586a797129f1d8914f0
jefrychiedi <jefrychiedi1999@gmail.com>
2023-04-09 00:19:44 +0700

    add Gin

--- main.go
+++ main.go
@@ -3,9 +3,24 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// Membuat instance router Gin
+	r := gin.Default()
+
+	// Menambahkan route GET
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello, world!",
+		})
+	})
+
+	// Menjalankan server HTTP
+	r.Run(":8080")
+
 	now := time.Now()
 	fmt.Printf("\n%s ⚡️ You are already connected to Golang!\n\n", now.Format("2006-01-02 15:04:05"))
 }
