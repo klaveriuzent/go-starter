1f3de13d843faa35908cc281f0aba1731f787608
jefrychiedi <jefrychiedi1999@gmail.com>
2023-04-08 23:53:44 +0700

    import time

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	now := time.Now()
+	fmt.Printf("\n%s ⚡️ You are already connected to Golang!\n\n", now.Format("2006-01-02 15:04:05"))
 }
